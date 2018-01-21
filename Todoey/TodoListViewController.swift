7b10f5f855dee9874a85d7eb81bf88e2e5b73a97
Bell Petchpol <bell_2530@hotmail.com>
2018-01-21 16:41:02 +0700

    Data persist using user default methods

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike","Try cocacola","Try Kooi"]
+    var itemArray = ["Find Mike","Try cocacola","Try Kooi"]
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
+            itemArray = items
+        }
     }
 
     //MARK - Tableview Datasource Methods
@@ -53,6 +58,34 @@ class TodoListViewController: UITableViewController {
         }
     }
 
+    //MARK - Add New Item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            // What will happen when user click Add Item Button on UiAlert
+            
+            self.itemArray.append(textField.text!)
+            
+            // save data to userdefault
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
+            self.tableView.reloadData()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
     
 
 }
